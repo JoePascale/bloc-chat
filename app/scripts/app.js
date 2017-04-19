c8c4dfc3126874659b469c0d68c91efa38ad6292
JoePascale <joe.m.pascale@gmail.com>
2017-04-19 14:28:52 -0700

    user story: list chat rooms completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,10 +1,20 @@
 (function() {
-    
-    $stateProvider
-         .state('main', {
-             templateUrl: '/index.html'
-         });
+    function config($stateProvider, $locationProvider) {
+        $locationProvider
+            .html5Mode({
+                enabled: true,
+                requireBase: false
+            });
+        
+        $stateProvider
+            .state('main', {
+                url: '/',
+                controller: 'RoomCtrl as room',
+                templateUrl: './index.html'
+            });
+    }
     
     angular
         .module('blocChat', ['ui.router', 'Firebase'])
+        .config(config);
 })();
