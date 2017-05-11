c6a7fd6da4b19d49c73f8c39ce39e5587fb1e889
JoePascale <joe.m.pascale@gmail.com>
2017-05-10 20:55:43 -0700

    updated submission for checkpoint

--- app/scripts/controllers/RoomCtrl.js
+++ app/scripts/controllers/RoomCtrl.js
@@ -3,7 +3,17 @@
         this.rooms = Room.all;
 
         //container to hold list of messages to the right of the list of available chat rooms
-        //this.activeRoom = room.onClick() in Room.rooms;
+        this.activeRoom = function(roomObj) {
+          console.log(roomObj.$id);
+          this.currentRoomName = roomObj.name;
+        };
+
+        /*this.activeMessages = function(messageObj) {
+          console.log(messageObj);
+          this.currentMessage = messageObj.getByRoomId;
+        };
+        */
+
     }
 
     angular
--- app/scripts/services/Message.js
+++ app/scripts/services/Message.js
@@ -3,13 +3,11 @@
     var Message = {};
     var ref = firebase.database().ref().child("messages");
 
-    //chain the child method with orderbyChild, targeting roomId
-    ref.orderbyChild("roomId");
-
+    //set up query to match and display messages with same roomId as the activeRoom selected
     Message.getByRoomId = function(roomId) {
       // Filter the messages by their room ID.
       ref.orderByChild('roomId').equalTo('roomId').on('activeRoom', function(snapshot) {
-        console.log(snapshot.val());
+        return snapshot.val();
       });
     };
 
